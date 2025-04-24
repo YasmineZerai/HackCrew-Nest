4f033efdca8f7f18b4aeae6b77e8986cd227f906
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-24 12:12:14 +0100

    import Team entitiy in typeorm config

--- backend/config/typeorm.config.ts
+++ backend/config/typeorm.config.ts
@@ -1,6 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { User } from 'src/user/entities/user.entity';
+import { Team } from 'src/team/entities/team.entity';
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'mysql',
@@ -9,7 +10,7 @@ export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOption
   username: configService.get<string>('DB_USERNAME'),
   password: configService.get<string>('DB_PASSWORD'),
   database: configService.get<string>('DB_NAME'),
-  entities: [User],
+  entities: [User,Team],
   synchronize: false,
   migrationsRun:true,
   autoLoadEntities:true
