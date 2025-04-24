e7be8d0b80513ebf663a226207756baaa7b6352c
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-24 22:01:05 +0100

    sse controller

---
+++ backend/src/sse/sse.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Param, Post, Sse } from '@nestjs/common';
+import { SseService } from './sse.service';
+import { Observable } from 'rxjs';
+
+@Controller('sse')
+export class SseController {
+    constructor(private readonly sseService: SseService) {}
+    @Sse(':id')
+    getEvents(
+      @Param('id') id: string,
+    ): Observable<{ data: any; event?: string }> {
+      return this.sseService.connect(id);
+    }
+    @Post('notify-user')
+    notifyUser(@Body() body ) {
+      this.sseService.notifyUser(body.userId, body.message,body.event);
+    }
+}
