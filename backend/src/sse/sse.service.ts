4b36a9779e60abaaa8cdff8fe703791c0820d2e5
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-24 21:55:59 +0100

    connect function

--- backend/src/sse/sse.service.ts
+++ backend/src/sse/sse.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ConnectedClient } from './interface/connected-client.interface';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SseService {
+    private clients : ConnectedClient[]=[]
 
+    connect(userId: string): Observable<{ data: any; event?: string }> {
+        return new Observable((subscriber) => {
+          const client: ConnectedClient = { userId,subscriber };
+          this.clients.push(client);
     
+          subscriber.add(() => {
+            this.clients = this.clients.filter(c => c !== client);
+          });
+        });
+      }
+
 
 
 }
