082db5eec1a36d2f9ab27be7b0326795283b0355
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-24 11:47:49 +0100

    type orm config + needed packages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from 'config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: typeOrmConfig,
+  }),UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
