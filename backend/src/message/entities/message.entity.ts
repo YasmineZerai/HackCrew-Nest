abd83fdcd6335964fc3977ac808cbed05b4214d7
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-26 00:23:38 +0100

    relation between message and team

--- backend/src/message/entities/message.entity.ts
+++ backend/src/message/entities/message.entity.ts
@@ -1,4 +1,5 @@
 import { Timestamp } from "src/common/entities/timestamp.entity";
+import { Team } from "src/team/entities/team.entity";
 import { User } from "src/user/entities/user.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
@@ -12,6 +13,9 @@ export class Message extends Timestamp {
 
   @ManyToOne(()=>User,user=>user.messages)
   sender : User
+
+  @ManyToOne(()=>Team,team=>team.messages)
+  team : Team
 
 
 
