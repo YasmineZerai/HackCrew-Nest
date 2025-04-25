bc8952011e527dcabac95f1dc73e125144199030
chedli01 <chedli.masmoudi01@gmail.com>
2025-04-25 22:48:23 +0100

    add relation between user and ressources

--- backend/src/ressource/entities/ressource.entity.ts
+++ backend/src/ressource/entities/ressource.entity.ts
@@ -1,4 +1,5 @@
 import { Team } from "src/team/entities/team.entity";
+import { User } from "src/user/entities/user.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('ressources')
@@ -15,5 +16,8 @@ export class Ressource {
 
     @ManyToOne(()=>Team,team=>team.ressources)
     team : Team
+
+    @ManyToOne(()=>User,user=>user.ressources)
+    user:User
 
 }
