ed6ed01e12ca8316576a0314243eea662fe97424
YasmineZerai <zeraiyasmine7@gmail.com>
2025-05-04 22:28:33 +0100

    integrating the auth in the message controller

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -9,39 +9,37 @@ import {
   UsePipes,
 } from '@nestjs/common';
 import { MessageService } from './message.service';
-// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import {
   CreateMessageDto,
   CreateMessageSchema,
 } from '@src/core/zod-schemas/create-message.schema';
 import { Request } from 'express';
 import { HttpZodPipe } from '@src/core/pipes/http-zod-validation.pipes';
+import { ConnectedUser } from '@src/auth/decorators/user.decorator';
+import { User } from '@src/user/entities/user.entity';
+import { JwtAuthGuard } from '@src/auth/guards/jwt.guard';
 
 @Controller('messages')
-// @UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard)
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
   @Post()
   @UsePipes(new HttpZodPipe(CreateMessageSchema))
   async create(
-    @Use() req: Request,
+    @ConnectedUser() user: User,
     @Body() createMessageDto: CreateMessageDto,
-  ): Promise<MessageResponseDto> {
+  ) {
     const message = await this.messageService.createMessage(
-      req.user,
+      user,
       createMessageDto,
     );
-    return;
+    return message;
   }
 
   @Get('team/:teamId')
-  async getTeamMessages(
-    @Param('teamId') teamId: number,
-  ): Promise<MessageResponseDto[]> {
+  async getTeamMessages(@Param('teamId') teamId: number) {
     const messages = await this.messageService.getTeamMessages(teamId);
-    return Promise.all(
-      messages.map((message) => this.messageService.mapToResponseDto(message)),
-    );
+    return messages;
   }
 }
--- backend/src/message/message.service.ts
+++ backend/src/message/message.service.ts
@@ -2,30 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Message } from './entities/message.entity';
-import { CreateMessageDto } from './dtos/create-message.dto';
 import { User } from '../user/entities/user.entity';
-import { TeamService } from '../team/team.service';
+import { CreateMessageDto } from '@src/core/zod-schemas/create-message.schema';
+// import { TeamService } from '../team/team.service';
 
 @Injectable()
 export class MessageService {
   constructor(
     @InjectRepository(Message)
     private readonly messageRepository: Repository<Message>,
-    private readonly teamService: TeamService,
+    // private readonly teamService: TeamService,
   ) {}
 
-  async createMessage(
-    sender: User,
-    createMessageDto: CreateMessageDto,
-  ): Promise<Message> {
-    const team = await this.teamService.findOne(createMessageDto.teamId);
-    const message = this.messageRepository.create({
-      content: createMessageDto.content,
-      sender,
-      team,
-    });
-
-    return await this.messageRepository.save(message);
+  async createMessage(sender: User, createMessageDto: CreateMessageDto) {
+    // const team = await this.teamService.findOne(createMessageDto.teamId);
+    // const message = this.messageRepository.create({
+    //   content: createMessageDto.content,
+    //   sender,
+    //   team,
+    // });
+    // return await this.messageRepository.save(message);
   }
 
   async getTeamMessages(teamId: number): Promise<Message[]> {
