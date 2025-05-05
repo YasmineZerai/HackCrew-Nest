833f620946399092604ea3bb5edb7c00db74aa7e
Youssef Fadhloun <youssef.fadhloun@thundercode.ai>
2025-05-05 01:10:05 +0100

    feat: crud resource management

--- backend/src/ressource/ressource.module.ts
+++ backend/src/ressource/ressource.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RessourcesController } from './ressource.controller';
+import { RessourcesService } from './ressource.service';
+import { Ressource } from './entities/ressource.entity';
+import { Team } from '../team/entities/team.entity';
+import { User } from '../user/entities/user.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Ressource, Team, User])],
+  controllers: [RessourcesController],
+  providers: [RessourcesService],
+})
 export class RessourceModule {}
