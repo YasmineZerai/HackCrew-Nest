f1448860166f1ebbd4f132f5548adfb22ae87c59
YasmineZerai <zeraiyasmine7@gmail.com>
2025-05-07 21:06:46 +0100

    added the join team room service

--- backend/src/socket/session/session.service.ts
+++ backend/src/socket/session/session.service.ts
@@ -2,6 +2,9 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { WsException } from '@nestjs/websockets';
+import { JWT_SECRET } from '@src/auth/constants/constants';
+import { TeamService } from '@src/team/team.service';
+import { User } from '@src/user/entities/user.entity';
 import { Socket } from 'socket.io';
 
 interface UserSocket {
@@ -14,15 +17,19 @@ export class SessionService {
   private connectedUsers: Map<string, string[]> = new Map();
   private socketToUser: Map<string, string> = new Map();
 
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly teamService: TeamService,
+  ) {}
 
   authenticate(client: Socket) {
-    const token = client.handshake.query.token as string;
+    const token = client.request.headers.authorization as string;
     if (!token) {
       throw new WsException('Invalid token');
     }
 
-    const payload = this.jwtService.verify(token);
+    const payload = this.jwtService.verify(token, { secret: JWT_SECRET });
+    console.log(payload.user);
     if (!payload.sub) {
       throw new WsException('Invalid token');
     }
@@ -63,5 +70,17 @@ export class SessionService {
 
   isUserOnline(userId: string): boolean {
     return this.connectedUsers.has(userId);
+  }
+  async joinTeamsRooms(client: Socket, user: { id: number; username: string }) {
+    const teams = await this.teamService.getTeamsByUserId(user.id);
+    teams.map((team) => {
+      client.join(`team_${team.id}`);
+      client
+        .to(`team_${team.id}`)
+        .emit('member_connected', {
+          message: `${user.username} is connected`,
+          userId: user.id,
+        });
+    });
   }
 }
--- backend/src/socket/socket.module.ts
+++ backend/src/socket/socket.module.ts
@@ -3,8 +3,10 @@ import { SocketService } from './socket.service';
 import { SocketGateway } from './socket.gateway';
 import { SessionService } from './session/session.service';
 import { JwtService } from '@nestjs/jwt';
+import { TeamModule } from '@src/team/team.module';
 
 @Module({
   providers: [SocketService, SocketGateway, SessionService, JwtService],
+  imports: [TeamModule],
 })
 export class SocketModule {}
