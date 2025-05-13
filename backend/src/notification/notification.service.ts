a4abaa288347e0ce2671ba2190bab96f3eea5d6c
chedli01 <chedli.masmoudi01@gmail.com>
2025-05-13 11:28:12 +0100

    notificationService extends GenericService

--- backend/src/notification/notification.service.ts
+++ backend/src/notification/notification.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { GenericService } from '@src/common/services/generic.service';
+import { Notification } from './entities/notification.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService extends GenericService<Notification> {
+    constructor(
+        @InjectRepository(Notification)
+        private readonly notificationRepository: Repository<Notification>,
+      ) {
+            super(notificationRepository)
+    
+      }
+
+    
+}
