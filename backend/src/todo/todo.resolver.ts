d3f232219127cd62c7b2312e68af98fdfbdc8a5c
chedli01 <chedli.masmoudi01@gmail.com>
2025-06-01 14:11:59 +0100

    document format

--- backend/src/todo/todo.resolver.ts
+++ backend/src/todo/todo.resolver.ts
@@ -5,25 +5,24 @@ import { TodoStatus } from '@src/enum/todo-status.enum';
 
 @Resolver()
 export class TodoResolver {
-    constructor(private readonly todoService : TodoService){}
+  constructor(private readonly todoService: TodoService) {}
 
+  @Query(() => [Todo])
+  getAllTodos(): Promise<Todo[]> {
+    return this.todoService.findAllTodos();
+  }
+  @Query(() => Todo)
+  getOneTodo(@Args('id', { type: () => Int }) id: number) {
+    return this.todoService.findOneTodo(id);
+  }
 
-     @Query(() => [Todo])
-      getAllTodos(): Promise<Todo[]> {
-        return this.todoService.findAllTodos();
-      }
-    @Query(() => Todo)
-      getOneTodo(@Args('id', { type: () => Int }) id: number) {
-        return this.todoService.findOneTodo(id);
-      }
-    
-       @Query(() => [Todo])
-async getTodoByTeam(
-  @Args('teamId', { type: () => Int }) teamId: number,
-  @Args('status', { type: () => TodoStatus, nullable: true }) status?: TodoStatus,
-  @Args('userId',{type:()=>Int,nullable:true}) userId? : number
-): Promise<Todo[]> {
-  return this.todoService.findTodoByTeam(teamId,userId, status);
-}
-
+  @Query(() => [Todo])
+  async getTodoByTeam(
+    @Args('teamId', { type: () => Int }) teamId: number,
+    @Args('status', { type: () => TodoStatus, nullable: true })
+    status?: TodoStatus,
+    @Args('userId', { type: () => Int, nullable: true }) userId?: number,
+  ): Promise<Todo[]> {
+    return this.todoService.findTodoByTeam(teamId, userId, status);
+  }
 }
